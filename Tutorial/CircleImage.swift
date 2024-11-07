49dd463f63a01d707ec256db6be7c19cc5619b5f
kim0hyeon <dbfwjs991105@hanyang.ac.kr>
2024-11-07 11:34:45 +0900

    Xcode ios app develop tutorial

---
+++ Tutorial/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Tutorial
+//
+//  Created by 김영현 on 11/7/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Tutorial/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Tutorial
+//
+//  Created by 김영현 on 11/7/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}

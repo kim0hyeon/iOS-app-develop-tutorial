763b0c744747a4f831e20a4f94bba814f4daa527
kim0hyeon <dbfwjs991105@hanyang.ac.kr>
2024-11-07 09:18:44 +0900

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by 김영현 on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TutorialTests/TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialTests.swift
+//  TutorialTests
+//
+//  Created by 김영현 on 11/7/24.
+//
+
+import Testing
+@testable import Tutorial
+
+struct TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
